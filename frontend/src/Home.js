6bc5c4fe5c14bb4bd0704aa378e5a365eb80ba3f
Ayush Setpal <as8675@g.rit.edu>
2024-04-17 19:10:42 -0400

    Landing page added

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import LandingImage from './assets/landing-image.png'
 
 const Home = () => {
   return (
-    <div>
-      <h1>High on World!</h1>
+    <div className='main-section-container'>
+      <div className='content'>
+      <h1 className='title'>Inventory Pro</h1>
+      <p className='description'>
+        Streamline your stock control with our intuitive Inventory Management System, designed to simplify order fulfillment and optimize supply chain efficiency
+      </p>
+      </div>
+      <div className="image-container">
+        <img src={LandingImage} alt="Inventory Management Visualization" className="image" />
+      </div>
     </div>
+    
   );
 }
 
