3077ca7f16db6fa08ff953cb7d4665ab4e1e1b0a
Jheel <noreply@scienceto.com>
2024-04-18 12:56:10 -0400

    Rectify cors import

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-var cors = require('cors');
+const cors = require('cors');
 const db = require("./models/models");
 
 const productRoutes = require('./routes/productRoutes');
