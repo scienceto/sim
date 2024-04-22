2c02d19333daac92b6b070112aeea3ccf7802751
uzairmukadam <uzair.mukadam1999@gmail.com>
2024-04-22 19:04:13 -0400

    Basic Suppliers page implemented

--- frontend/src/components/console/Supplier.js
+++ frontend/src/components/console/Supplier.js
@@ -1,11 +1,77 @@
+import { useState, useEffect } from "react";
+import { apiBaseUrl } from "../../config";
+
 const Supplier = () => {
+  const [suppliers, setSuppliers] = useState([]);
+  const [formData, setFormData] = useState({
+    app_user: "",
+    status: ""
+  });
+
+  const fetchSuppliers = async () => {
+    try {
+      const response = await fetch(`${apiBaseUrl}/suppliers`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch suppliers");
+      }
+      const data = await response.json();
+      setSuppliers(data);
+    } catch (error) {
+      console.error("Error fetching suppliers:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSuppliers();
+  }, []);
+
     return (
-      <div>
+      <div className="product-container">
+      <div className="product-table">
         <h2>Suppliers</h2>
-        <p>This is the content for Suppliers.</p>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Metadata</th>
+              {/* Add more table headers as needed */}
+            </tr>
+          </thead>
+          {/* <tbody>
+            {suppliers.map((product) => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>{product.category}</td>
+                <td>{product.description}</td>
+              </tr>
+            ))}
+          </tbody> */}
+        </table>
       </div>
+      {/* <div className="product-form">
+        <h2>Add Suppliers</h2>
+        <form>
+          <div>
+            <label>:</label>
+            <input type="text" name="name" value={formData.name} />
+          </div>
+          <div>
+            <label>Category:</label>
+            <input type="text" name="category" value={formData.category} />
+          </div>
+          <div>
+            <label>Description:</label>
+            <textarea name="description" value={formData.description}  />
+          </div>
+          <button type="submit">Add Suppliers</button>
+        </form>
+      </div> */}
+    </div>
     );
   };
   
-  export default Supplier;
+  export default Warehouse;
   
