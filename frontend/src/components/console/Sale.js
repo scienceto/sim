3b79b41b9ed421206aeb34e058df443718fb21b5
uzairmukadam <uzair.mukadam1999@gmail.com>
2024-04-22 18:47:28 -0400

    Basic Sale page implemented

--- frontend/src/components/console/Sale.js
+++ frontend/src/components/console/Sale.js
@@ -1,9 +1,76 @@
+import { useState, useEffect } from "react";
+import { apiBaseUrl } from "../../config";
+
 const Sale = () => {
+  const [sales, setSales] = useState([]);
+  const [formData, setFormData] = useState({
+    customer: "",
+    app_user: "",
+    status: ""
+  });
+
+  const fetchSales = async () => {
+    try {
+      const response = await fetch(`${apiBaseUrl}/sales`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.json();
+      setSales(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSales();
+  }, []);
+
     return (
-      <div>
+      <div className="product-container">
+      <div className="product-table">
         <h2>Sales</h2>
-        <p>This is the content for Sales.</p>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Customer</th>
+              <th>App_User</th>
+              <th>Status</th>
+              {/* Add more table headers as needed */}
+            </tr>
+          </thead>
+          {/* <tbody>
+            {sale.map((product) => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>{product.category}</td>
+                <td>{product.description}</td>
+              </tr>
+            ))}
+          </tbody> */}
+        </table>
       </div>
+      {/* <div className="product-form">
+        <h2>Add Sales</h2>
+        <form>
+          <div>
+            <label>:</label>
+            <input type="text" name="name" value={formData.name} />
+          </div>
+          <div>
+            <label>Category:</label>
+            <input type="text" name="category" value={formData.category} />
+          </div>
+          <div>
+            <label>Description:</label>
+            <textarea name="description" value={formData.description}  />
+          </div>
+          <button type="submit">Add Sale</button>
+        </form>
+      </div> */}
+    </div>
     );
   };
   
