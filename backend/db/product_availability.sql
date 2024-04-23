5a1abadb69d472b01c0fe447f11c4a34ae970290
uzairmukadam <uzair.mukadam1999@gmail.com>
2024-04-22 22:40:48 -0400

    Inventory retrieval modified

--- backend/db/product_availability.sql
+++ backend/db/product_availability.sql
@@ -1,4 +1,4 @@
-SELECT w.name AS warehouse_name, p.name AS product_name, pi.quantity AS total_quantity
+SELECT pi.id as id, w.name AS warehouse_name, p.name AS product_name, pi.quantity AS quantity
 FROM product_inventory pi
 JOIN warehouse w ON pi.warehouse = w.id
 JOIN product p ON pi.product = p.id
